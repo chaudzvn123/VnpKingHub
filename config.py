60191d61c11ee162ea4392375157474883a74851
chaudzvn123 <jwjanwnuhhshsj@gmail.com>
2025-08-12 18:28:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-
+ADMIN_UIDS = ["123456789012345678"]  # Thay UID Discord admin ở đây
+WEBHOOK_URL = "https://discord.com/api/webhooks/xxx/yyy"  # Thay webhook thật
+TOKEN = "YOUR_DISCORD_BOT_TOKEN"
